a3ab8fa224e3fa7ec358635cca6e0395dbe1c10f
Jimin Park <pjm950@gmail.com>
2013-10-06 23:51:14 +0900

    Added all record tags in body.

--- generate.js
+++ generate.js
@@ -4,6 +4,6 @@ var parse = function(file){return format.parser.parse(fs.readFileSync("./format/
 var hwp_node_record_file = fs.readFileSync("./format/hwp-node-record.js", 'utf-8');
 
 var code = parse('node') + '\n' + parse('record');
-console.log(code);
+// console.log(code);
 code = hwp_node_record_file.replace('// CODE //', code);
 fs.writeFileSync("./lib/hwpdata.js", code, 'utf-8');
