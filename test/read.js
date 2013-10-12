f6b2f856ec4027860b19caaa978c1a8f7a0061c9
Jimin Park <pjm950@gmail.com>
2013-10-12 20:10:12 +0900

    Added new test file

--- test/read.js
+++ test/read.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var hwp = require('../');
 
-const file = 'test_1';
+const file = 'test_3';
 
 var test = function(ok){
 	hwp.open('./test/files/'+file+'.hwp', function(err, doc){
